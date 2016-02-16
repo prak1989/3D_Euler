f512cb0d02fa2fb76fb58501528c028b9bc60204
prak1989 <prakash.raj819@gmail.com>
2016-02-16 15:52:53 +0530

    Volume works

--- euler_fvm.f90
+++ euler_fvm.f90
@@ -1,75 +1,94 @@
-module variables
+module Variables
+implicit none
+contains
+	!***********************************Compute volume*****************************************!
+	subroutine Volume(B,H,A_iph, A_jph, A_kph,Cell_Volume)
+
+		implicit none
+		double precision, dimension(1:3) :: B, H, d1, d2, A_iph, A_jph, A_kph
+		double precision :: Cell_Volume, Vol, d4
+
+		Vol = 0.0d0
+		d2(1) = B(1) - H(1)
+		d2(2) = B(2) - H(2)
+		d2(3) = B(3) - H(3)
+
+		d1(1) = A_iph(1)
+		d1(2) = A_iph(2)
+		d1(3) = A_iph(3)
+
+		call dot_prod(d1,d2,d4)
+		Vol = Vol + d4
+
+		d1(1) = A_jph(1)
+		d1(2) = A_jph(2)
+		d1(3) = A_jph(3)
+
+		call dot_prod(d1,d2,d4)
+		Vol = Vol + d4
+
+		d1(1) = A_kph(1)
+		d1(2) = A_kph(2)
+		d1(3) = A_kph(3)
+
+		call dot_prod(d1,d2,d4)
+		Vol = Vol + d4
+
+		Cell_Volume = (1.0d0/3.0d0)*Vol 
+	end subroutine Volume
+
+	!******Cross Product***********!
+	subroutine cross_prod(d1,d2,d3)
+
+		implicit none
+		double precision, dimension(1:3) :: d1, d2, d3	
+		d3(1) = ((d1(2)*d2(3))- (d2(2)*d1(3)))
+		d3(2) = -((d1(1)*d2(3))- (d2(1)*d1(3)))
+		d3(3) = ((d1(1)*d2(2))- (d2(1)*d1(2)))
+		
+	end subroutine cross_prod
+
+
+	!********Dot Product***********!
+	subroutine dot_prod(d1,d2,d4)
+
+		implicit none
+		double precision, dimension(1:3) :: d1, d2
+		double precision :: d4 
+		d4 = d1(1)*d2(1) + d1(2)*d2(2) + d1(3)*d2(3)
+
+	end subroutine dot_prod
+	
+end module Variables
+
+
+program Euler
+	use Variables
+	implicit none	
+
 	integer :: i,j,k	
-	real*4 :: dx, dy, dz
-	integer :: Nx = 10, Ny = 10, Nz = 10
-	real*4 :: g = 1.4, XUpper, XLower, YUpper, YLower, ZUpper, ZLower, d4 
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: ds
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: x, y, z
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: xc,yc,zc
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: rho,u,v,w,pr,c	
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: u1,u2,u3,u4,u5
- 	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: f1,f2,f3,f4,f5
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: g1,g2,g3,g4,g5
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: h1,h2,h3,h4,h5
-	real*4, dimension(1:3) :: A,B,C,D,E,F,G,H,d1,d2,d3
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2,1:3) :: A_iph,A_imh,A_jph,A_jmh,A_kph,A_kmh
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2,1:3) :: n_iph,n_imh,n_jph,n_jmh,n_kph,n_kmh
-	real*4, dimension(0:Nx+2,0:Ny+2,0:Nz+2,1:3) :: Fc_iph,Fc_jph,Fc_kph,Fc_imh,Fc_jmh,Fc_kmh  
-	real*4 :: A_IPH, A_IMH, A_JPH, A_JMH, A_KPH, A_KMH
-	!real*8, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: s1,s2,s3,s4,s5,s6
-	!real*8, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: n1,n2,n3,n4,n5,n6
-end module variables
-
-
-
-module var_declare
-	use variables
-	implicit none
-	u1(i,j,k) = rho(i,j,k)
-	u2(i,j,k) = rho(i,j,k) * u(i,j,k)
-	u3(i,j,k) = rho(i,j,k) * v(i,j,k)
-	u4(i,j,k) = rho(i,j,k) * w(i,j,k)
-	u5(i,j,k) = (p(i,j,k)/(g-1.0)) + ((rho(i,j,k)*(u(i,j,k)**2 + v(i,j,k)**2 + w(i,j,k)**2))/2.0)
-
-	f1(i,j,k) = rho(i,j,k) * u(i,j,k)
-	f2(i,j,k) = (rho(i,j,k) * u(i,j,k)**2) + p(i,j,k))
-	f3(i,j,k) = rho(i,j,k) * u(i,j,k) * v(i,j,k)
-	f4(i,j,k) = rho(i,j,k) * u(i,j,k) * w(i,j,k)
-	f5(i,j,k) = (u5(i,j,k) + p(i,j,k)) * u(i,j,k)
-
-	g1(i,j,k) = rho(i,j,k) * v(i,j,k)
-	g2(i,j,k) = rho(i,j,k) * v(i,j,k) * u(i,j,k)
-	g3(i,j,k) = (rho(i,j,k) * v(i,j,k)**2) + p(i,j,k))          
-	g4(i,j,k) = rho(i,j,k) * v(i,j,k) * w(i,j,k)
-	g5(i,j,k) = (u5(i,j,k) + p(i,j,k)) * v(i,j,k)
-
-	h1(i,j,k) = rho(i,j,k) * v(i,j,k)
-	h2(i,j,k) = rho(i,j,k) * w(i,j,k) * u(i,j,k)
-	h3(i,j,k) = rho(i,j,k) * w(i,j,k) * v(i,j,k)          
-	h4(i,j,k) = (rho(i,j,k) * w(i,j,k)**2) + p(i,j,k))  
-	h5(i,j,k) = (u5(i,j,k) + p(i,j,k)) * w(i,j,k)
-
-	!******************** del_x, del_y, del_z **********************!
+	double precision :: dx, dy, dz
+	integer,parameter :: Nx = 10, Ny = 10, Nz = 10
+	double precision, parameter :: gamma = 1.40d0, XUpper = 1.0d0, XLower = 0.0d0, YUpper = 1.0d0, YLower = 0.0d0, ZUpper = 1.0d0, ZLower = 0.0d0
+	double precision, dimension(1:nx, 1:ny, 1:nz) :: vol
+	double precision, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: x, y, z
+	double precision, dimension(0:Nx+2,0:Ny+2,0:Nz+2) :: xc,yc,zc
+	double precision, dimension(1:3) :: A,B,C,D,E,F,G,H
+	double precision, dimension(1:3) :: d1,d2,d3
+	double precision, dimension(0:Nx+2,0:Ny+2,0:Nz+2,1:3) :: A_iph,A_imh,A_jph,A_jmh,A_kph,A_kmh
+	double precision, dimension(0:Nx+2,0:Ny+2,0:Nz+2,1:3) :: n_iph,n_imh,n_jph,n_jmh,n_kph,n_kmh
+	double precision, dimension(0:Nx+2,0:Ny+2,0:Nz+2,1:3) :: Fc_iph,Fc_jph,Fc_kph,Fc_imh,Fc_jmh,Fc_kmh  
+	double precision :: surf_mag_iph, surf_mag_imh, surf_mag_jph, surf_mag_jmh, surf_mag_kph, surf_mag_kmh, d4, Cell_Volume
 	
 	dx = (XUpper - XLower)/Nx
 	dy = (YUpper - YLower)/Ny
 	dz = (ZUpper - ZLower)/Nz
 
-	
-end module var_declare
-
-
-
-program Euler
-	use variables
-	use var_declare
-	implicit none	
-	
 	!******************* Grid generation (node points) ******************!
 	
-	do i = 0,Nx+2
+	do k = 0,Nz+2
 		do j = 0,Ny+2
-			do k = 0, Nz+2
+			do i = 0, Nx+2
 				x(i,j,k) = (i-1)*dx
 				y(i,j,k) = (j-1)*dy
 				z(i,j,k) = (k-1)*dz
@@ -80,21 +99,21 @@ program Euler
 
 	!************************ Center points *****************************!
 	
-	do i = 0,Nx+1
+	do k = 0,Nz+1
 		do j = 0,Ny+1
-			do k = 0, Nz+1
-				xc(i,j,k) = 0.5*(x(i+1,j,k) + x(i,j,k))
-				yc(i,j,k) = 0.5*(y(i,j+1,k) + y(i,j,k))
-				zc(i,j,k) = 0.5*(z(i,j,k+1) + z(i,j,k))
+			do i = 0, Nx+1
+				xc(i,j,k) = 0.50d0*(x(i+1,j,k) + x(i,j,k))
+				yc(i,j,k) = 0.50d0*(y(i,j+1,k) + y(i,j,k))
+				zc(i,j,k) = 0.50d0*(z(i,j,k+1) + z(i,j,k))
 			end do
 		end do
 	end do
 
 	
 	!********************* Defining the vertices ************************!
-	do i = 0,Nx+1
+	do k = 0,Nz+1
 		do j = 0,Ny+1
-			do k = 0, Nz+1
+			do i = 0, Nx+1
 				A(1) = x(i+1,j,k+1)
 				A(2) = y(i+1,j,k+1)
 				A(3) = z(i+1,j,k+1)
@@ -139,16 +158,16 @@ program Euler
 				d2(2) = B(2) - D(2)
 				d2(3) = B(3) - D(3)
 
-				call cross_prod(d1,d2)
-				A_iph(i,j,k,1) = 0.5*d3(1)
-				A_iph(i,j,k,2) = 0.5*d3(2)
-				A_iph(i,j,k,3) = 0.5*d3(3)
+				call cross_prod(d1,d2,d3)
+				A_iph(i,j,k,1) = 0.50d0*d3(1)
+				A_iph(i,j,k,2) = 0.50d0*d3(2)
+				A_iph(i,j,k,3) = 0.50d0*d3(3)
 
-				A_IPH = (A_iph(i,j,k,1)**2 + A_iph(i,j,k,2)**2 + A_iph(i,j,k,3)**2)**0.5 !***magnitude***!
+				surf_mag_iph = (A_iph(i,j,k,1)**2 + A_iph(i,j,k,2)**2 + A_iph(i,j,k,3)**2)**0.50d0 !***magnitude***!
 			
-				n_iph(i,j,k,1) = A_iph(i,j,k,1)/A_IPH
-				n_iph(i,j,k,2) = A_iph(i,j,k,2)/A_IPH
-				n_iph(i,j,k,3) = A_iph(i,j,k,3)/A_IPH
+				n_iph(i,j,k,1) = A_iph(i,j,k,1)/surf_mag_iph
+				n_iph(i,j,k,2) = A_iph(i,j,k,2)/surf_mag_iph
+				n_iph(i,j,k,3) = A_iph(i,j,k,3)/surf_mag_iph
 
 			!******* Area and unit normal @ i-1/2 *******!
 				d1(1) = G(1) - E(1)
@@ -159,16 +178,16 @@ program Euler
 				d2(2) = F(2) - H(2)
 				d2(3) = F(3) - H(3)
 
-				call cross_prod(d1,d2)
-				A_imh(i,j,k,1) = 0.5*d3(1)
-				A_imh(i,j,k,2) = 0.5*d3(2)
-				A_imh(i,j,k,3) = 0.5*d3(3)
+				call cross_prod(d1,d2,d3)
+				A_imh(i,j,k,1) = 0.50d0*d3(1)
+				A_imh(i,j,k,2) = 0.50d0*d3(2)
+				A_imh(i,j,k,3) = 0.50d0*d3(3)
 
-				A_IMH = (A_imh(i,j,k,1)**2 + A_imh(i,j,k,2)**2 + A_imh(i,j,k,3)**2)**0.5 !***magnitude***!
+				surf_mag_imh = (A_imh(i,j,k,1)**2 + A_imh(i,j,k,2)**2 + A_imh(i,j,k,3)**2)**0.50d0 !***magnitude***!
 			
-				n_imh(i,j,k,1) = A_imh(i,j,k,1)/A_IMH
-				n_imh(i,j,k,2) = A_imh(i,j,k,2)/A_IMH
-				n_imh(i,j,k,3) = A_imh(i,j,k,3)/A_IMH
+				n_imh(i,j,k,1) = A_imh(i,j,k,1)/surf_mag_imh
+				n_imh(i,j,k,2) = A_imh(i,j,k,2)/surf_mag_imh
+				n_imh(i,j,k,3) = A_imh(i,j,k,3)/surf_mag_imh
 
 			!******* Area and unit normal @ j+1/2 *******!
 			 	d1(1) = F(1) - C(1)
@@ -179,16 +198,16 @@ program Euler
 				d2(2) = B(2) - G(2)
 				d2(3) = B(3) - G(3)
 
-				call cross_prod(d1,d2)
-				A_jph(i,j,k,1) = 0.5*d3(1)
-				A_jph(i,j,k,2) = 0.5*d3(2)
-				A_jph(i,j,k,3) = 0.5*d3(3)
+				call cross_prod(d1,d2,d3)
+				A_jph(i,j,k,1) = 0.50d0*d3(1)
+				A_jph(i,j,k,2) = 0.50d0*d3(2)
+				A_jph(i,j,k,3) = 0.50d0*d3(3)
 
-				A_JPH = (A_jph(i,j,k,1)**2 + A_jph(i,j,k,2)**2 + A_jph(i,j,k,3)**2)**0.5 !***magnitude***!
+				surf_mag_jph = (A_jph(i,j,k,1)**2 + A_jph(i,j,k,2)**2 + A_jph(i,j,k,3)**2)**0.50d0 !***magnitude***!
 			
-				n_jph(i,j,k,1) = A_jph(i,j,k,1)/A_JPH
-				n_jph(i,j,k,2) = A_jph(i,j,k,2)/A_JPH
-				n_jph(i,j,k,3) = A_jph(i,j,k,3)/A_JPH
+				n_jph(i,j,k,1) = A_jph(i,j,k,1)/surf_mag_jph
+				n_jph(i,j,k,2) = A_jph(i,j,k,2)/surf_mag_jph
+				n_jph(i,j,k,3) = A_jph(i,j,k,3)/surf_mag_jph
 			
 			!******* Area and unit normal @ j-1/2 *******!
 				d1(1) = E(1) - D(1)
@@ -199,16 +218,16 @@ program Euler
 				d2(2) = A(2) - H(2)
 				d2(3) = A(3) - H(3)
 
-				call cross_prod(d1,d2)
-				A_jmh(i,j,k,1) = 0.5*d3(1)
-				A_jmh(i,j,k,2) = 0.5*d3(2)
-				A_jmh(i,j,k,3) = 0.5*d3(3)
+				call cross_prod(d1,d2,d3)
+				A_jmh(i,j,k,1) = 0.50d0*d3(1)
+				A_jmh(i,j,k,2) = 0.50d0*d3(2)
+				A_jmh(i,j,k,3) = 0.50d0*d3(3)
 
-				A_JMH = (A_jmh(i,j,k,1)**2 + A_jmh(i,j,k,2)**2 + A_jmh(i,j,k,3)**2)**0.5 !***magnitude***!
+				surf_mag_jmh = (A_jmh(i,j,k,1)**2 + A_jmh(i,j,k,2)**2 + A_jmh(i,j,k,3)**2)**0.50d0 !***magnitude***!
 			
-				n_jmh(i,j,k,1) = A_jmh(i,j,k,1)/A_JMH
-				n_jmh(i,j,k,2) = A_jmh(i,j,k,2)/A_JMH
-				n_jmh(i,j,k,3) = A_jmh(i,j,k,3)/A_JMH	
+				n_jmh(i,j,k,1) = A_jmh(i,j,k,1)/surf_mag_jmh
+				n_jmh(i,j,k,2) = A_jmh(i,j,k,2)/surf_mag_jmh
+				n_jmh(i,j,k,3) = A_jmh(i,j,k,3)/surf_mag_jmh	
 				
 			!******* Area and unit normal @ k+1/2 *******!
 			 	d1(1) = B(1) - E(1)
@@ -219,16 +238,16 @@ program Euler
 				d2(2) = F(2) - A(2)
 				d2(3) = F(3) - A(3)
 
-				call cross_prod(d1,d2)
-				A_kph(i,j,k,1) = 0.5*d3(1)
-				A_kph(i,j,k,2) = 0.5*d3(2)
-				A_kph(i,j,k,3) = 0.5*d3(3)
+				call cross_prod(d1,d2,d3)
+				A_kph(i,j,k,1) = 0.50d0*d3(1)
+				A_kph(i,j,k,2) = 0.50d0*d3(2)
+				A_kph(i,j,k,3) = 0.50d0*d3(3)
 
-				A_KPH = (A_kph(i,j,k,1)**2 + A_kph(i,j,k,2)**2 + A_kph(i,j,k,3)**2)**0.5 !***magnitude***!
+				surf_mag_kph = (A_kph(i,j,k,1)**2 + A_kph(i,j,k,2)**2 + A_kph(i,j,k,3)**2)**0.50d0 !***magnitude***!
 			
-				n_kph(i,j,k,1) = A_kph(i,j,k,1)/A_KPH
-				n_kph(i,j,k,2) = A_kph(i,j,k,2)/A_KPH
-				n_kph(i,j,k,3) = A_kph(i,j,k,3)/A_KPH
+				n_kph(i,j,k,1) = A_kph(i,j,k,1)/surf_mag_kph
+				n_kph(i,j,k,2) = A_kph(i,j,k,2)/surf_mag_kph
+				n_kph(i,j,k,3) = A_kph(i,j,k,3)/surf_mag_kph
 
 			!******* Area and unit normal @ k-1/2 *******!
 				d1(1) = C(1) - H(1)
@@ -239,113 +258,37 @@ program Euler
 				d2(2) = G(2) - D(2)
 				d2(3) = G(3) - D(3)
 
-				call cross_prod(d1,d2)
-				A_kmh(i,j,k,1) = 0.5*d3(1)
-				A_kmh(i,j,k,2) = 0.5*d3(2)
-				A_kmh(i,j,k,3) = 0.5*d3(3)
+				call cross_prod(d1,d2,d3)
+				A_kmh(i,j,k,1) = 0.50d0*d3(1)
+				A_kmh(i,j,k,2) = 0.50d0*d3(2)
+				A_kmh(i,j,k,3) = 0.50d0*d3(3)
 
-				A_KMH = (A_kmh(i,j,k,1)**2 + A_kmh(i,j,k,2)**2 + A_kmh(i,j,k,3)**2)**0.5 !***magnitude***!
+				surf_mag_kmh = (A_kmh(i,j,k,1)**2 + A_kmh(i,j,k,2)**2 + A_kmh(i,j,k,3)**2)**0.50d0 !***magnitude***!
 			
-				n_kmh(i,j,k,1) = A_kmh(i,j,k,1)/A_KMH
-				n_kmh(i,j,k,2) = A_kmh(i,j,k,2)/A_KMH
-				n_kmh(i,j,k,3) = A_kmh(i,j,k,3)/A_KMH
+				n_kmh(i,j,k,1) = A_kmh(i,j,k,1)/surf_mag_kmh
+				n_kmh(i,j,k,2) = A_kmh(i,j,k,2)/surf_mag_kmh
+				n_kmh(i,j,k,3) = A_kmh(i,j,k,3)/surf_mag_kmh
+
+				call Volume(B,H,A_iph(i,j,k,1:3), A_jph(i,j,k,1:3), A_kph(i,j,k,1:3),Cell_Volume)
+				vol(i,j,k) = Cell_Volume
 			end do
 		end do
 	end do
 
-
-
-
-!*********************Computing face centroids*******************************!
-	do i = 0,Nx+1
-		do j = 0,Ny+1
-			do k = 0, Nz+1
-				Fc_iph(i,j,k,1) = 0.25*(x(i+1,j,k+1) + x(i+1,j+1,k+1) + x(i+1,j+1,k) + x(i+1,j,k))
-				Fc_iph(i,j,k,2) = 0.25*(y(i+1,j,k+1) + y(i+1,j+1,k+1) + y(i+1,j+1,k) + y(i+1,j,k))
-				Fc_iph(i,j,k,3) = 0.25*(z(i+1,j,k+1) + z(i+1,j+1,k+1) + z(i+1,j+1,k) + z(i+1,j,k))
-
-				Fc_jph(i,j,k,1) = 0.25*(x(i+1,j+1,k+1) + x(i+1,j+1,k) + x(i,j+1,k) + x(i,j+1,k+1))
-				Fc_jph(i,j,k,2) = 0.25*(y(i+1,j+1,k+1) + y(i+1,j+1,k) + y(i,j+1,k) + y(i,j+1,k+1))
-				Fc_jph(i,j,k,3) = 0.25*(z(i+1,j+1,k+1) + z(i+1,j+1,k) + z(i,j+1,k) + z(i,j+1,k+1))
-	
-				Fc_kph(i,j,k,1) = 0.25*(x(i+1,j+1,k+1) + x(i+1,j,k+1) + x(i,j,k+1) + x(i,j+1,k+1))
-				Fc_kph(i,j,k,2) = 0.25*(y(i+1,j+1,k+1) + y(i+1,j,k+1) + y(i,j,k+1) + y(i,j+1,k+1))
-				Fc_kph(i,j,k,3) = 0.25*(z(i+1,j+1,k+1) + z(i+1,j,k+1) + z(i,j,k+1) + z(i,j+1,k+1))
-			
-				Fc_imh(i,j,k,1) = 0.25*(x(i,j,k+1) + x(i,j+1,k+1) + x(i,j+1,k) + x(i,j,k))
-				Fc_imh(i,j,k,2) = 0.25*(y(i,j,k+1) + y(i,j+1,k+1) + y(i,j+1,k) + y(i,j,k))
-				Fc_imh(i,j,k,3) = 0.25*(z(i,j,k+1) + z(i,j+1,k+1) + z(i,j+1,k) + z(i,j,k))
-
-				Fc_jmh(i,j,k,1) = 0.25*(x(i,j,k+1) + x(i,j,k) + x(i+1,j,k) + x(i+1,j,k+1))
-				Fc_jmh(i,j,k,2) = 0.25*(y(i,j,k+1) + y(i,j,k) + y(i+1,j,k) + y(i+1,j,k+1))
-				Fc_jmh(i,j,k,3) = 0.25*(z(i,j,k+1) + z(i,j,k) + z(i+1,j,k) + z(i+1,j,k+1))
-			
-				Fc_kmh(i,j,k,1) = 0.25*(x(i,j,k) + x(i+1,j,k) + x(i+1,j+1,k) + x(i,j+1,k))
-				Fc_kmh(i,j,k,2) = 0.25*(y(i,j,k) + y(i+1,j,k) + y(i+1,j+1,k) + y(i,j+1,k))
-				Fc_kmh(i,j,k,3) = 0.25*(z(i,j,k) + z(i+1,j,k) + z(i+1,j+1,k) + z(i,j+1,k))
+	open(unit = 45, file="volume.dat")	
+	do k = 1, nz
+		do j = 1, ny
+			do i = 1, nx
+				write(45,'(1E30.18)') vol(i,j,k)
 			end do
 		end do
 	end do
-
-
-!***********************************Compute volume*****************************************!
-subroutine Volume
-	Vol = 0.0
-	d2(1) = B(1) - H(1)
-	d2(2) = B(2) - H(2)
-	d2(3) = B(3) - H(3)
-
-	d1(1) = A_iph(i,j,k,1)
-	d1(2) = A_iph(i,j,k,2)
-	d1(3) = A_iph(i,j,k,3)
-
-	call dot_prod(d1,d2)
-	Vol = Vol + d4
-
-	d1(1) = A_jph(i,j,k,1)
-	d1(2) = A_jph(i,j,k,2)
-	d1(3) = A_jph(i,j,k,3)
-
-	call dot_prod(d1,d2)
-	Vol = Vol + d4
-
-	d1(1) = A_jph(i,j,k,1)
-	d1(2) = A_jph(i,j,k,2)
-	d1(3) = A_jph(i,j,k,3)
-
-	call dot_prod(d1,d2)
-	Vol = Vol + d4
-
-	Cell_Volume = (1.0/3.0)*Vol 
+	close(45)
 
 end program Euler
 
 
 
 
-
-
-
-subroutine cross_prod(d1,d2)
-	use variables
-	use var_declare
-	implicit none
-		
-	d3(1) = ((d1(2)*d2(3))- (d2(2)*d1(3)))
-	d3(2) = -((d1(1)*d2(3))- (d2(1)*d1(3)))
-	d3(3) = ((d1(1)*d2(2))- (d2(1)*d1(2)))
-		
-end subroutine cross_prod
-
-
-
-subroutine dot_prod(d1,d2)
-	use variables
-	use var_declare
-	implicit none
-
-	d4 = d1(1)*d2(1) + d1(2)*d2(2) + d1(3)*d2(3)
-
-end subroutine dot_prod
 
 
